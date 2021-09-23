73faa72b6555e6469df265a2f47e1207ee32e4f9
Niko D <nikodojan@live.de>
2021-09-23 09:35:44 +0200

    added MC class

---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// Represents a vehicle of type: Car
+    /// </summary>
+    public class MC
+    {
+        /// <summary>
+        /// The license number plate of the vehicle used.
+        /// </summary>
+        public string LicensePlate { get; set; }
+
+        /// <summary>
+        /// The date and time of the ticket purchase with this vehicle.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Method to retrieve the ticket price for this vehicle.
+        /// </summary>
+        /// <returns>Price as double</returns>
+        public double Price()
+        {
+            return 125;
+        }
+        
+        /// <summary>
+        /// Method to retrieve the vehicle type.
+        /// </summary>
+        /// <returns>Vehicle type as string.</returns>
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
