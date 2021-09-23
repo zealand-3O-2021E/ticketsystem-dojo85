0b89e2cd3a6ac29cb1de32aee2edfa1e6ef7fda9
Niko D <nikodojan@live.de>
2021-09-23 09:29:42 +0200

    car class created

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class Car
+    {
+        public string  LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Price()
+        {
+            return 240;
+        }
+
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ TicketSystemTests/CarTests.cs
@@ -0,0 +1,31 @@
+using ClassLibraryTicketSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketSystemTests
+{
+    [TestClass]
+    public class CarTests
+    {
+        [TestMethod]
+        public void Price_Test()
+        {
+            Car car = new();
+            double expected = 240;
+
+            double actual = car.Price();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VehicleType_Test()
+        {
+            Car car = new();
+            string expected = "Car";
+
+            string actual = car.VehicleType();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
