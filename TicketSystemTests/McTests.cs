6cbd34c54e4ea790ff3933d1052913746f35dd61
Niko D <nikodojan@live.de>
2021-09-23 09:39:22 +0200

    added mc class tests

---
+++ TicketSystemTests/McTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryTicketSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketSystemTests
+{
+    [TestClass]
+    public class McTests
+    {
+        [TestMethod]
+        public void Price_Test()
+        {
+            MC mc = new();
+            double expected = 125;
+
+            double actual = mc.Price();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void VehicleType_Test()
+        {
+            MC mc = new();
+            string expected = "MC";
+
+            string actual = mc.Vehicle();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
